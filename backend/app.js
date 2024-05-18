a790f571cb06b68628b0d403ee367032a73829af
Brian <brian.kieken@gmail.com>
2024-05-18 11:01:21 +0200

    MongoDB and Models Schema

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,91 @@
 const express = require('express')
+const mongoose = require('mongoose');
+const uri = "mongodb+srv://bk:-$@cluster0.iofkxrr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const Book = require('./models/Book');
+
+mongoose.connect(uri,{ useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log('Connexion à MongoDB réussie !'))
+.catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express()
 
+app.use(express.json());
+
 app.use((req, res, next) => {
-    console.log('Requête reçue !');
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+//add book
+app.post('/api/books', (req, res, next) => {
+  delete req.body._id;
+  const book = new Book({
+    ...req.body
   });
-  
-  app.use((req, res, next) => {
-    res.status(201);
-    next();
-  });
-  
-  app.use((req, res, next) => {
-    res.json({ message: 'Votre requête a bien été reçue !' });
-    next();
-  });
-  
-  app.use((req, res, next) => {
-    console.log('Réponse envoyée avec succès !');
-  });
+  book.save()
+    .then(() => res.status(201).json({ message: 'Livre enregistré !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+//get one book
+app.get('/api/books/:id', (req, res, next) => {
+  Book.findOne({ _id: req.params.id })
+    .then(book => res.status(200).json(book))
+    .catch(error => res.status(400).json({ error }));
+});
+
+//get all books
+app.get('/api/books', (req, res, next) => {
+  Book.find()
+    .then(books => res.status(200).json(books))
+    .catch(error => res.status(400).json({ error }));
+});
 
 module.exports = app
+
+/*
+const books = [
+  {
+    userId: 1,
+    title: 'Le cahier d\'enquête de Franck Thilliez',
+    author: 'auteur book',
+    imageUrl: 'https://static.fnac-static.com/multimedia/PE/Images/FR/NR/1d/4c/00/16796701/1507-1/tsp20240501081153/Le-cahier-d-enquete-de-Franck-Thilliez.jpg',
+    year: 2012,
+    genre: 'fiction',
+    ratings: [{
+      userId: 1,
+      grade: 3,
+    }],
+    averageRating: 3,
+  },
+  {
+    userId: 1,
+    title: 'Le Médecin malgré lui',
+    author: 'auteur book',
+    imageUrl: 'https://static.fnac-static.com/multimedia/PE/Images/FR/NR/41/ba/04/309825/1507-1/tsp20231214072416/Le-Medecin-malgre-lui.jpg',
+    year: 2012,
+    genre: 'fiction',
+    ratings: [{
+      userId: 1,
+      grade: 2,
+    }],
+    averageRating: 2,
+  },
+  {
+    userId: 1,
+    title: 'Harry Potter à l\'école des sorciers',
+    author: 'auteur book',
+    imageUrl: 'https://static.fnac-static.com/multimedia/PE/Images/FR/NR/ac/91/7c/8163756/1507-1/tsp20240313164207/Harry-Potter-a-l-ecole-des-sorciers.jpg',
+    year: 2016,
+    genre: 'fiction',
+    ratings: [{
+      userId: 1,
+      grade: 5,
+    }],
+    averageRating: 5,
+  }
+];
+res.status(200).json(books);*/
