e0e236717c5eec805994c89c3c06181f401b2df2
Brian <brian.kieken@gmail.com>
2024-06-01 11:46:14 +0200

    Finalisation Projet

--- backend/controllers/books.js
+++ backend/controllers/books.js
@@ -16,14 +16,6 @@ exports.createBook = (req, res, next) => {
   .catch(error => { res.status(400).json({ error })})
 }
 
-/*delete req.body._id;
-const book = new Book({
-  ...req.body
-});
-book.save()
-  .then(() => res.status(201).json({ message: 'Livre enregistré !'}))
-  .catch(error => res.status(400).json({ error }));*/
-
 exports.getBook = (req, res, next) => {
   Book.findOne({ _id: req.params.id })
     .then(book => res.status(200).json(book))
--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,16 +1,15 @@
 const express = require('express')
 const router = express.Router()
 
-const auth = require('../middleware/auth');
-const multer = require('../middleware/multer-config')
+const auth      = require('../middleware/auth');
+const multer    = require('../middleware/multer-config')
 const resizeImg = require('../middleware/resize-img');
-const bookCtrl = require('../controllers/books')
+const bookCtrl  = require('../controllers/books')
 
 router.post('/', auth, multer, resizeImg, bookCtrl.createBook);
 router.get('/bestrating', bookCtrl.getBestRating);
 router.get('/:id', bookCtrl.getBook);
 router.get('/', bookCtrl.getAllBooks);
-
 
 router.put('/:id', auth, multer, resizeImg, bookCtrl.updateBook);
 router.delete('/:id', auth, bookCtrl.deleteBook);
